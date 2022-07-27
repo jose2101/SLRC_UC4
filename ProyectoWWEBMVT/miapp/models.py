b66b835f7d787959f2313d3eabba955e18c8242d
Elias Reyes <elias.rey2526@gmail.com>
2022-07-27 07:05:55 -0500

    Parte 2 - Tabla Estudiante - OK

--- ProyectoWWEBMVT/miapp/models.py
+++ ProyectoWWEBMVT/miapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Estudiante(models.Model):
+    codigo = models.CharField(max_length=10)
+    dni = models.CharField(max_length=8)
+    nombre = models.CharField(max_length=30)
+    apepat = models.CharField(max_length=30)
+    apemat = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=9)
+    estado = models.CharField(max_length=20)
